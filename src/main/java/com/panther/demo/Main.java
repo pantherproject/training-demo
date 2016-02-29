f176a10997306640de66d57d0caa2fbb352b2a87
panther.dongdong <panther.dongdong@qunar.com>
2016-02-29 12:45:17 +0800

    create test demo

---
+++ src/main/java/com/panther/demo/Main.java
@@ -0,0 +1,11 @@
+package com.panther.demo;
+
+/**
+ * test demo
+ * Created by panther.dongdong on 2016/2/29.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test...");
+    }
+}
