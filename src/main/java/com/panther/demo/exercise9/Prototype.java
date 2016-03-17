5652be53d6d3d8d2784066f424a2c50c53ef34b7
panther.dongdong <panther.dongdong@qunar.com>
2016-03-17 11:15:38 +0800

    原型模式[对象的深浅复制]

--- src/main/java/com/panther/demo/exercise9/Prototype.java
+++ src/main/java/com/panther/demo/exercise9/Prototype.java
@@ -14,13 +14,13 @@ public class Prototype implements Cloneable, Serializable {
     private SerializableObject serializableObject;
 
     //浅复制
-    public Object clone() throws CloneNotSupportedException {
+    public Prototype clone() throws CloneNotSupportedException {
         Prototype prototype = (Prototype) super.clone();
         return prototype;
     }
 
     //深复制
-    public Object deepClone() throws IOException, ClassNotFoundException {
+    public Prototype deepClone() throws IOException, ClassNotFoundException {
         //写入当前对象的二进制流
         ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteOutputStream);
@@ -29,7 +29,7 @@ public class Prototype implements Cloneable, Serializable {
         //读出二进制流产生新对象
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteOutputStream.toByteArray());
         ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-        return objectInputStream.readObject();
+        return (Prototype) objectInputStream.readObject();
     }
 
 
@@ -49,17 +49,18 @@ public class Prototype implements Cloneable, Serializable {
         this.serializableObject = serializableObject;
     }
 
-    class SerializableObject implements Serializable {
+    static class SerializableObject implements Serializable {
 
         private static final long serialVersionUID = -7728443971130605297L;
     }
 
-    public static void test(int a) {
-        System.out.println(a);
+    public static void main(String[] args) throws CloneNotSupportedException, IOException, ClassNotFoundException {
+        Prototype prototype = new Prototype();
+        prototype.setSerializableObject(new SerializableObject()); //设置引用类型对象
+        Prototype prototype1 = prototype.clone(); //浅复制
+        Prototype prototype2 = prototype.deepClone(); //深复制
+        System.out.println(prototype.getSerializableObject() == prototype1.getSerializableObject()); //true
+        System.out.println(prototype.getSerializableObject() == prototype2.getSerializableObject()); //false
     }
 
-    public static void main(String[] args) {
-        short a = 'a';
-        Prototype.test(a);
-    }
 }
